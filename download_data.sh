63006938dc794f41c01a6892db9ab80253609f8a
yqeen-xue <kq24393@bristol.ac.uk>
2024-12-02 03:35:33 +0000

    Add files via upload
    nano times into 10000

--- download_data.sh
+++ download_data.sh
@@ -6,7 +6,7 @@ LOGFILE="logs/download.log"
 > $LOGFILE
 
 echo "Downloading PMIDs..." >> $LOGFILE
-curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=%22long%20covid%22&retmax=10" > data/raw/pmids.xml 2>> $LOGFILE
+curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=%22long%20covid%22&retmax=10000" > data/raw/pmids.xml 2>> $LOGFILE
 
 if [ $? -ne 0 ]; then
     echo "Failed to download PMIDs." >> $LOGFILE
